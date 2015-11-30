acfee3b1e39c75a085aa34416e95a8cb30ffe36c
Gustavo Sampaio <gbritosampaio@gmail.com>
2015-11-30 17:31:18 -0300

    config updated

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 const config = {
-  CONSUMER_KEY: 'kO7h098h4NPcKlnoScmWm37bX',
-  CONSUMER_SECRET: 'Z4kkjqv2ylBydswZHe65ZnYhQ50uCqvi9jGMAs7GVojzPkPUN0',
+  CONSUMER_KEY: 'CONSUMER_KEY',
+  CONSUMER_SECRET: 'CONSUMER_SECRET',
 
-  ACCESS_TOKEN: '2824854044-g59MZcyBmhq7wu6IRnLbzbtIcAZSXrpGYWIjUxn',
-  ACCESS_TOKEN_SECRET: '5sVVqCt3K59mZMcTNhixQ8pEO2asxM2gott0sm1WSRIkU',
+  ACCESS_TOKEN: 'ACCESS_TOKEN',
+  ACCESS_TOKEN_SECRET: 'ACCESS_TOKEN_SECRET',
 
   MARKOV_ORDER: 3,
 
-  CORPUS: 'corpus/all.json',
-  CORPUS_DIGEST: 'corpus/all.model',
+  CORPUS: 'corpus/test.json',
+  CORPUS_DIGEST: 'corpus/test.model',
 
   BOT_NAME: 'botanado',
 
